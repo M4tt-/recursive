3c3981feeb57309ff0741794b18e675f8f0721e4
Matt Runyon <M4tt-@users.noreply.github.com>
2017-03-23 21:10:01 -0400

    Added numDig() and sum2Dig()

--- recursive.c
+++ recursive.c
@@ -11,6 +11,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int numDig(int n){
+	int count = 0;
+	if(n!=0){
+		++count;
+		return count+numDig(n/10);
+	}else{
+		return count;
+	}
+}
+
 int factorial(int n){
 	if(n == 0)
 		return 1;
@@ -48,9 +58,45 @@ int sumDig(int n){
        return 0;
 }
 
+int sum2Dig(int n){
+	int nd = numDig(n);
+	if(nd%2!=0 && nd>1){
+		int rem = n%10;
+		n = n/10;
+		if (n != 0)
+	       return  rem+(n%10)+((n/10)%10)*10 + sum2Dig(n/100);
+	    else
+	       return 0;
+	}else if(nd%2!=0 && nd<=1)
+		return n;
+	else
+		if (n != 0)
+	       return (n%10)+((n/10)%10)*10 + sum2Dig(n/100);
+	    else
+	       return 0;
+}
+
 int main(void){
 
-	printf("Testing recursiveFactorial() ... \n");
+	printf("Testing numDig()...\n");
+	printf("Case 1: Calling numDig(0)\n");
+	printf("Expected Result: 0\n");
+	printf("Actual Result:   %d\n\n", numDig(0));
+	printf("Case 2: Calling numDig(6)\n");
+	printf("Expected Result: 1\n");
+	printf("Actual Result:   %d\n\n", numDig(6));
+	printf("Case 3: Calling numDig(16)\n");
+	printf("Expected Result: 2\n");
+	printf("Actual Result:   %d\n\n", numDig(16));
+	printf("Case 4: Calling numDig(314465730)\n");
+	printf("Expected Result: 8\n");
+	printf("Actual Result:   %d\n\n", numDig(31446573));
+	printf("Case 5: Calling numDig(2161)\n");
+	printf("Expected Result: 4\n");
+	printf("Actual Result:   %d\n\n", numDig(2161));
+	printf("\n");
+	
+	printf("Testing factorial() ... \n");
 	printf("Case 1: Calling factorial(0).\n");
 	printf("Expected Result: 1\n");
 	printf("Actual Result:   %d\n",factorial(0));
@@ -140,6 +186,29 @@ int main(void){
 	printf("Case 4: Calling sumDig(29465)\n");
 	printf("Expected Result: 26\n");
 	printf("Actual Result:   %d\n", sumDig(29465));
+	
+		
+	printf("Testing sum2Dig()...\n");
+	printf("Case 1: Calling sum2Dig(0)\n");
+	printf("Expected Result: 0\n");
+	printf("Actual Result:   %d\n\n", sum2Dig(0));
+	printf("Case 2: Calling sum2Dig(6)\n");
+	printf("Expected Result: 6\n");
+	printf("Actual Result:   %d\n\n", sum2Dig(6));
+	printf("Case 3: Calling sum2Dig(36)\n");
+	printf("Expected Result: 36\n");
+	printf("Actual Result:   %d\n\n", sum2Dig(36));
+	printf("Case 4: Calling sum2Dig(2946)\n");
+	printf("Expected Result: 75\n");
+	printf("Actual Result:   %d\n\n", sum2Dig(2946));
+	printf("Case 5: Calling sum2Dig(294644)\n");
+	printf("Expected Result: 119\n");
+	printf("Actual Result:   %d\n\n", sum2Dig(294644));
+	printf("Case 6: Calling sum2Dig(29465)\n");
+	printf("Expected Result: 80\n");
+	printf("Actual Result:   %d\n", sum2Dig(29465));
+	printf("\n\n");
+	
 	printf("\n");
 
 	return 0;
